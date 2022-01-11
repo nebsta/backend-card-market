189252768276691c59ac6193e4758107ed5f9694
Benjamin Wallis <ben.nat.wallis@gmail.com>
2022-01-11 15:12:36 +0100

    setting up basic mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import express = require('express')
+import express = require('express');
+import mongo = require('mongodb');
+
+import dotenv = require('dotenv')
+dotenv.config();
+
+mongo.MongoClient.connect(process.env.MONGODB_CON, (err, client) => {
+  console.log(`connection done. Error: ${err}. Client: ${client}`)
+});
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
